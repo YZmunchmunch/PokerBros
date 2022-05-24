c60935817bc78abd4638e99e5e72ad9ca094ea44
Ang Yuze <86197080+YZmunchmunch@users.noreply.github.com>
2022-05-24 20:26:35 +0800

    Add files via upload

---
+++ PokerBros/PokerBros/View Controllers/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  PokerBros
+//
+//  Created by Ang Yuze on 20/5/22.
+//
+
+import UIKit
+import SwiftUI
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var signUpButton: UIButton!
+    
+    @IBOutlet weak var loginButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        setUpElements()
+    }
+    
+    func setUpElements() {
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
